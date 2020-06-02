0bfab7456eb1c1b8477b44d279d3c723cca7ef39
hemei <505349085@qq.com>
2020-06-02 20:59:17 +0800

    获取token

--- 20200531_work/get_tag.py
+++ 20200531_work/get_tag.py
@@ -6,9 +6,15 @@ class GetToken:
     def __init__(self,appid,secret):
         data={'grant_type':'client_credential','appid':appid,'secret':secret}
         self.response = requests.get('https://api.weixin.qq.com/cgi-bin/token?', params=data)
-        self.response.content.decode('utf-8')
+        self.response.encoding='utf-8'
+    def get_token(self):
+        content=str(self.response.text)
+        result=re.match('"access_token":"(.+)"',content)
+        return result.group(1)
 
-
+if __name__=="__main__":
+    a=GetToken('wx60536c088aee3040','f214d833f873d8cc1b38255eca0938d9')
+    print(a.get_token())
 # data={'grant_type':'client_credential','appid':'wx60536c088aee3040','secret':'f214d833f873d8cc1b38255eca0938d9'}
 # response=requests.get('https://api.weixin.qq.com/cgi-bin/token?',params=data)
 # print(response.content.decode('utf-8'))
